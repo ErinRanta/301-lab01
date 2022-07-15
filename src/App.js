46dd2d38945c773e29d8c1bdefd82dc2e4f9550b
Erin Ranta <erinran99@gmail.com>
2022-07-15 12:46:32 -0400

    cases

--- src/App.js
+++ src/App.js
@@ -1,55 +1,17 @@
-import React from 'react';
 import './App.css';
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
-// import ChosenBeast from './components/ChosenBeast.js';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import list from './data.json';
 
-class App extends React.Component{
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      beastList: list,
-      chosenBeast: {},
-      showModal: false,
-    }
-  }
-  handleShowModal = (beastName) => {
-    const chosenBeast = list.find(beast => beast.title === beastName);
-    this.setState({showModal: true, chosenBeast: chosenBeast});
-    console.log(chosenBeast.description);
-  };
-
-  handleExitModal = () => {
-    this.setState({showModal: false});
-  };
+function App() {
+  return (
+    <div className="App">
+      <Header></Header>
+      <Main></Main>
+      <Footer></Footer>
+    </div>
   
-  render() {
-    return (
-      <div className="App">
-        <Header />
-        <Main 
-          beastList={this.state.beastList}
-          handleShowModal={this.handleShowModal}
-        />
-        <ChosenBeast 
-          show = {this.state.showModal}
-          handleExit = {this.handleExitModal}
-          chosenBeast = {this.state.chosenBeast}
-        />
-        <Footer />
-      </div>
-    
-    );
-  }
+  );
 }
 
 export default App;
-
-
-
-
-
