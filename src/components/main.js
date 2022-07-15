3fd865ae951a2720e370ee4f4636478def09d647
Erin Ranta <erinran99@gmail.com>
2022-07-15 12:51:41 -0400

    finalizing case confusion

--- src/components/main.js
+++ src/components/main.js
@@ -1,24 +1,36 @@
 
-import HornedBeast from './hornedbeast.js'
-import Images from '../data.json';
+import { Component } from "react";
+import HornedBeast from './hornedbeast'
+import beastJSON from '../data.json';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
 
-
-const Main = () => {
-  let imagesArr = Images;
-
-  let displayArr = [];
-
-  imagesArr.forEach((element) => {
-    displayArr.push(<HornedBeast image_url={element.image_url} title={element.title} description={element.description}/>);
-  })
-
-return (
-  <main>{displayArr}</main>
-);}
-
+class Main extends Component{
+    constructor(){
+        super();
+        this.state = {beasts: beastJSON}
+    }
+    
+   
+    render(){
+        return(
+            <Container fluid>
+                <Row xs={1} sm={2}md={3} lg={4} xl={5}>
+                    {this.state.beasts.map(b => 
+                        <Col>
+                            key={b._id.toString()}
+                            <HornedBeast 
+                                title={b.title}
+                                image_url={b.image_url}
+                                description={b.description}
+                            />
+                        </Col>
+                    )}
+                </Row>  
+            </Container>
+        );
+    }
+}
 
 export default Main;
-
-
-
-
