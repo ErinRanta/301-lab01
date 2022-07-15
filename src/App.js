ba4d9534993996bb4246bfae938e3152aeaba023
Erin Ranta <erinran99@gmail.com>
2022-07-15 16:13:39 -0400

    modal

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import './App.css';
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
+import React from 'react';
+import './App.css';
+import Images from './data.json'
+import SelectedBeast from './components/selectbeast.js';
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-      <Main></Main>
-      <Footer></Footer>
-    </div>
-  
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {modalImgUrl: null, modalImgTitle: null, modalImgDesc: null, viewModal: false};
+  }
+
+  handleModalClick = (image) => {
+    this.setState({modalImgUrl: image.props.image_url, modalImgTitle: image.props.title, modalImgDesc: image.props.description, viewModal: true});
+  }
+
+  closeModal = () => {
+    this.setState({viewModal: false});
+  }
+
+  render() {
+      return (
+        <>
+        <Header />
+        <Main images={Images} handleModalClick={this.handleModalClick} />
+        <SelectedBeast imageUrl={this.state.modalImgUrl} imageTitle={this.state.modalImgTitle} imageDesc={this.state.modalImgDesc} closeModal={this.closeModal} viewModal={this.state.viewModal} />
+        <Footer />
+      </>
+      );
+    }
 }
 
 export default App;
