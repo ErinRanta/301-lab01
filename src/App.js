c352a9a0ea5f8bed6e01fa83044d69636792da54
Erin Ranta <erinran99@gmail.com>
2022-07-18 23:08:52 -0400

    final

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 
 
-
-
 import Header from './components/header.js';
 import Main from './components/main.js';
 import Footer from './components/footer.js';
@@ -10,24 +8,7 @@ import './App.css';
 import Images from './data.json'
 import SelectedBeast from './components/selectbeast.js';
 
-
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-       this.state = {modalImgUrl: null, modalImgTitle: null, modalImgDesc: null, viewModal: false};
-  }
-
-  handleModalClick = (image) => {
-    this.setState({modalImgUrl: image.props.image_url, modalImgTitle: image.props.title, modalImgDesc: image.props.description, viewModal: true});
-  }
-
-  closeModal = () => {
-    this.setState({viewModal: false});
-  }
-
-
-
-  class extends React.Component {
   constructor(props) {
     super(props);
     this.state = {modalImgUrl: null, modalImgTitle: null, modalImgDesc: null, viewModal: false};
@@ -41,16 +22,16 @@ class App extends React.Component {
     this.setState({viewModal: false});
   }
 
-
   render() {
       return (
-        <div className="App">
+        <>
         <Header />
         <Main images={Images} handleModalClick={this.handleModalClick} />
         <SelectedBeast imageUrl={this.state.modalImgUrl} imageTitle={this.state.modalImgTitle} imageDesc={this.state.modalImgDesc} closeModal={this.closeModal} viewModal={this.state.viewModal} />
         <Footer />
-      </div>      );
+      </>
+      );
     }
-  }
+}
 
 export default App;
--- src/components/main.js
+++ src/components/main.js
@@ -71,30 +71,3 @@ export default Main;
 
 
 
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import HornedBeast from "./hornedbeast.js";
-import React from 'react';
-
-class Main extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.images = props.images;
-  }
-
-  render () {
-
-    return (
-        <main>
-          <Container>
-            <Row>
-              {this.images.map(el => <HornedBeast image_url={el.image_url} title={el.title} description={el.description} handleModalClick={this.props.handleModalClick} />)}
-            </Row>
-          </Container>
-        </main>
-        );
-    }
-  }
-
-export default Main;
